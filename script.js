7797220137b793eab6c0c0d152896b8f6515e65a
robertferry <robertmferry@gmail.com>
2022-11-13 20:45:13 -0600

    enter button works

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
     function changeBackgroundColorRandom() {
       document.body.style.backgroundColor = arr[randomNum];
     }
+    
     butt1.addEventListener('click', randomWallpaper)
     let arr = ['red', 'blue', 'green', 'pink','purple', 'orange', 'yellow']
     let randomNum = Math.floor(Math.random()*arr.length)
@@ -17,13 +18,18 @@ function colorText() {
 function randomWallpaper() {
   document.body.style.backgroundImage =
   "url('https://source.unsplash.com/1600x900/?" + textBox.value + "')";
-  if (Event.key === 'Enter'){
-  document.body.style.backgroundColor = 'red';
-  }
+  
 }
 //⬆️⬆️⬆️⬆️CREATE WALLPAPER FROM SEARCH =-=-=-=-=-=-
-
-
-
+///⬇️⬇️⬇️ENTER BUTTON PRESSED WORKS NOW=-=-=-=-=
+textBox.addEventListener('keypress', (event)=>{
+  // event.keyCode or event.which  property will have the code of the pressed key
+  let keyCode = event ? event.keyCode : event.which;
+  // 13 points the enter key
+  if(keyCode === 13) { 
+    randomWallpaper();
+  } 
+});
+//⬆️⬆️⬆️⬆️ENTER BUTTON EVENT IN TEXTBOX=-=-=-=-
 
   })
